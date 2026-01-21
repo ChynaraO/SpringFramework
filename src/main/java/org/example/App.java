ede8a012c3de3dc1483d4ca603c209a962e31d71
ChynaraO <omurzakova_chyna@mail.ru>
2026-01-21 15:35:25 -0600

    Spring Bean xml config

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+
+//        Alien obj = new Alien();
+//        obj.code();
+
+        Alien obj = (Alien) context.getBean("alien");
+        obj.code();
+    }
+}
