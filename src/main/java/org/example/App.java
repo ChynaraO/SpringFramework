cf73dca76d5692dbecf75edcfc4378b50b353cd5
ChynaraO <omurzakova_chyna@mail.ru>
2026-01-22 17:31:54 -0600

    Java based config

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -11,12 +13,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compile();
 
-        Alien obj1 = (Alien) context.getBean("alien1");
 
-        System.out.println(obj1.getAge());
-        obj1.code();
+
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//
+//        Alien obj1 = context.getBean("alien1", Alien.class);
+//
+//        System.out.println(obj1.getAge());
+//        obj1.code();
+//
+//        Desktop obj2 = context.getBean( Desktop.class);
 
     }
 }
