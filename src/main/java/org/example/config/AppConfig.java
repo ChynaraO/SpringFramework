7337dfa0c35110e12757dcdf080a772efec5a224
ChynaraO <omurzakova_chyna@mail.ru>
2026-01-22 18:13:10 -0600

    Java based config - @Primary(defines bean as a primary)

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -8,13 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public Alien alien(@Qualifier("com2") Computer com){
+    public Alien alien( Computer com){
         Alien obj = new Alien();
         obj.setAge(25);
         obj.setCom(com);
@@ -29,6 +30,7 @@ public class AppConfig {
     }
 
     @Bean
+    @Primary
     public Laptop laptop(){
         return new Laptop();
     }
